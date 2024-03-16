64427b132c5ec66397c03732d87b78f90315c888
NguyenTrieuTu1212 <143092693+NguyenTrieuTu1212@users.noreply.github.com>
2024-03-16 10:30:16 +0700

    Connect firebase to flutter
    - Use firebase CLI

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:app_car_booking/Auth/login_screen.dart';
 import 'package:app_car_booking/Auth/sign_up_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import 'firebase_options.dart';
 
 
 
-void main(){
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 

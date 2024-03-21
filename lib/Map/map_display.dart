95fcf960cc4bf473f319fe645e7c0bf222b92bbd
NguyenTrieuTu1212 <143092693+NguyenTrieuTu1212@users.noreply.github.com>
2024-03-21 21:15:11 +0700

    Add themes for app

--- lib/Map/map_display.dart
+++ lib/Map/map_display.dart
@@ -1,7 +1,9 @@
 
 import 'dart:async';
+import 'dart:convert';
 import 'package:app_car_booking/Global/global_var.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 
@@ -16,6 +18,22 @@ class MapSampleState extends State<MapSample> {
 
   final Completer<GoogleMapController> googleMapCompleter = Completer<GoogleMapController>();
   GoogleMapController? controllerGoogleMap;
+
+  updateThemeMap(GoogleMapController controller){
+    getDataFromJson("themes/map_theme_night.json").then((value) => setStyleMap(controller, value));
+  }
+
+
+  Future<String> getDataFromJson(String path) async{
+    ByteData byteData = await rootBundle.load(path);
+    var list = byteData.buffer.asUint8List(byteData.offsetInBytes, byteData.lengthInBytes);
+    return utf8.decode(list);
+  }
+
+  setStyleMap(GoogleMapController controller,String dataMapStyle){
+    controller.setMapStyle(dataMapStyle);
+  }
+
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
@@ -27,6 +45,7 @@ class MapSampleState extends State<MapSample> {
             initialCameraPosition: googleInitPos,
             onMapCreated: (GoogleMapController mapController){
               controllerGoogleMap = mapController;
+              updateThemeMap(controllerGoogleMap!);
               googleMapCompleter.complete(controllerGoogleMap);
             },
           )

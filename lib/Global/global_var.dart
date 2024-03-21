87ef4c1c9f07f6450e167bcd945cb4e65a573293
NguyenTrieuTu1212 <143092693+NguyenTrieuTu1212@users.noreply.github.com>
2024-03-21 20:33:17 +0700

    Add map in home page app

--- lib/Global/global_var.dart
+++ lib/Global/global_var.dart
@@ -1,2 +1,9 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 String userName ="";
 String googleMapKey ="AIzaSyBbMNaYG54dqrE4TH03TETRp7MyXOCuuu8";
+
+const CameraPosition googleInitPos = CameraPosition(
+  target: LatLng(37.42796133580664, -122.085749655962),
+  zoom: 14.4746,
+);
--- lib/Map/map_display.dart
+++ lib/Map/map_display.dart
@@ -1,5 +1,6 @@
 
 import 'dart:async';
+import 'package:app_car_booking/Global/global_var.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -12,35 +13,27 @@ class MapSample extends StatefulWidget {
 }
 
 class MapSampleState extends State<MapSample> {
-  final Completer<GoogleMapController> _controller =
-  Completer<GoogleMapController>();
 
-  static const CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
-
-  static const CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
-
+  final Completer<GoogleMapController> googleMapCompleter = Completer<GoogleMapController>();
+  GoogleMapController? controllerGoogleMap;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: GoogleMap(
-        mapType: MapType.normal,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
+    return  Scaffold(
+      body: Stack(
+        children: [
+          GoogleMap(
+            mapType: MapType.normal,
+            myLocationEnabled: true,
+            initialCameraPosition: googleInitPos,
+            onMapCreated: (GoogleMapController mapController){
+              controllerGoogleMap = mapController;
+              googleMapCompleter.complete(controllerGoogleMap);
+            },
+          )
+        ],
       ),
     );
   }
 
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    await controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
-  }
+
 }
